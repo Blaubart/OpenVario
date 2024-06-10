3067e638dd24cfaaf8cdc55efbf24e2ce25eca56
August2111 <Info@FlapsOnline.de>
2024-06-10 16:57:41 +0200

    ### ShellScriptCommands.sh - add some shell commands (date)

--- meta-openvario/recipes-apps/ovmenu-ng-skripts/files/ShellScriptCommands.sh
+++ meta-openvario/recipes-apps/ovmenu-ng-skripts/files/ShellScriptCommands.sh
@@ -21,3 +21,19 @@ if 3 && 0; then echo "3 && 0 = true"; else echo "3 && 0 = false"; fi
 
 echo "-------------------------------------------------------------------------"
 echo "-------------------------------------------------------------------------"
+
+# remove a yocto project to make a new build:
+find . -type f -name "*Name*.*" -delete
+# find only:
+find . -type f -name "*Name*.*" 
+find . -type f -name "/opensoar*.*" 
+
+# kernel values
+## temperature:
+cat /sys/class/thermal/thermal_zone*/temp
+## frequency:
+cat /sys/devices/system/cpu/cpu*/cpufreq/cpuinfo_cur_freq
+
+# set datetime: this is setting the software clock only, the hardware clock
+# is untouched (and set the system clock at next boot
+date -s "2024-10-06 10:21"
